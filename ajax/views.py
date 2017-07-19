6c4cba95a5e7f2925e3b3ff4d20b244d6d2d8bb8
HSATAN <2499090390@qq.com>
2017-07-19 20:57:33 +0800

    添加数据库

--- ajax/views.py
+++ ajax/views.py
@@ -2,8 +2,5 @@ from django.shortcuts import render
 from . import models
 # Create your views here.
 def index(request):
-    articles_node=models.Article.objects.exclude(pk=10)
-    articles=[]
-    for article in articles_node:
-        articles.append(article)
+    articles=models.Article.objects.exclude(pk=10)
     return render(request,'ajax/index.html',{'articles':articles})
